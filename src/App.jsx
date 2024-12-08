8b3ad927bec14c6fd0395202979092f2d40661e5
Nikita Basargin <nbasargin@web.de>
2024-12-08 17:24:46 +0000

    remove unused components; update font and images

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@
 
 import React from "react";
 
-import About from "./Components/About";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
 
@@ -24,28 +21,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "John Doe",
+  title: "Some job title here",
 };
-
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
 
 const App = () => {
   return (
     <div id="main">
-      <Header />
       <Home name={siteProps.name} title={siteProps.title} />
-      <About />
       <Portfolio />
-      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
 };
